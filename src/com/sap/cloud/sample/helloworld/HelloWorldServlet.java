76597a687a86de46a26edd3b22389c9a11a3d41b
R <hariprasauth.r@sap.com>
2017-12-22 16:22:01 +0530

    Changes to mvn

--- src/com/sap/cloud/sample/helloworld/HelloWorldServlet.java
+++ src/com/sap/cloud/sample/helloworld/HelloWorldServlet.java
@@ -17,6 +17,5 @@ public class HelloWorldServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().println("<p>Hello World - Updated!</p>");
-        sda
     }
 }
